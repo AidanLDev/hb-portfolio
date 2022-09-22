9cd54f083ae2a5ae01b35f35fefaa8d65673935c
Aidan <aidan.lowson@davies-group.com>
2022-09-22 16:49:07 +0100

    Playing around with the website here and there

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify/functions';
-
 import react from '@astrojs/react';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harrybell.netlify.app/',
   output: 'server',
   adapter: netlify(),
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), react(), image()]
 });
