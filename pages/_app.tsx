5b5de17af980b143b4a3f4ed97ce4c89fac03a84
Aidan <dev@aidanlowson.com>
2023-02-28 00:08:28 +0000

    Adding fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,14 @@ import type { AppProps } from "next/app";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
 
+// Fonts and themeing
+import Fonts from "components/Fonts";
+import theme from "components/Theme";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   );
